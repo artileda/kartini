bc6a5e036f5528efb3142ff453ccd1185b355913
Elsa Purwani <47468136+ywnn@users.noreply.github.com>
2021-04-15 11:15:23 +0700

    feat: [WIP] CLI Interface

--- bin/main.ml
+++ bin/main.ml
@@ -1,2 +1,23 @@
+open Cmdliner
 
-let () = ();;
+
+(* Real Function *)
+let add =
+  ""
+;;
+
+(* Term Function *)
+let add_t = 
+  let doc = "Adding binary archive to system" in
+  Term.(const add),
+  Term.info "add" ~doc
+;;
+
+
+let info_t = 
+  let doc = "Simple Package Administrator" in
+  Term.(ret (const (fun _ -> `Help (`Pager, None)) $ const ())),
+  Term.info "kartini" ~version:"0.0.1" ~doc ~exits:Term.default_exits
+;;
+
+let () = Term.(exit @@ eval_choice info_t [add_t]);;
